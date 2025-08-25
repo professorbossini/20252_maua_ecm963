98f0330a5832964a24f63240e6433215543b7908
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-25 18:09:37 -0300

    learn(ap002-rn): encerra pg 39
    - fizemos a remocao
    - deixamos sem alert, pois nao funciona na web
    - foi proposto o exercio de usar um alert que funcione na web
    - alunos podem usar a sugestao que esta na apostila
    - tambem podem encontrar outros
    - nao podem usar IA para gerar codigo, mas podem usar para encontrar sugestoes

--- lembretes/App.tsx
+++ lembretes/App.tsx
@@ -1,16 +1,19 @@
-import { 
-  Button,
-  Pressable,
-  StyleSheet,
-  Text, 
-  TextInput, 
-  View } 
-from 'react-native'
 import {
   useState
 } from 'react'
+import {
+  Alert,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+}
+  from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
 
-interface Lembrete{
+interface Lembrete {
   id: string;
   texto: string;
 }
@@ -18,35 +21,91 @@ interface Lembrete{
 export default function App() {
   const [lembrete, setLembrete] = useState('')
   const [lembretes, setLembretes] = useState<Lembrete[]>([])
-//   - construir um objeto Lembrete com id igual à data atual do sistema e texto  igual ao valor
-// existente na variável de estado chamada lembrete
-// - utilizar o hook associado à lista de lembretes a fim de adicionar o lembrete à lista,
-// garantindo a atualização da tela
-// - atualizar a variável lembrete armazenando a cadeia vazia, limpando portando o campo em
-// que o usuário digita seus lembretes
+  //   - construir um objeto Lembrete com id igual à data atual do sistema e texto  igual ao valor
+  // existente na variável de estado chamada lembrete
+  // - utilizar o hook associado à lista de lembretes a fim de adicionar o lembrete à lista,
+  // garantindo a atualização da tela
+  // - atualizar a variável lembrete armazenando a cadeia vazia, limpando portando o campo em
+  // que o usuário digita seus lembretes
   const adicionar = () => {
     const novoLembrete: Lembrete = {
       id: Date.now().toString(),
       texto: lembrete
     }
-    setLembretes(lembretesAtuais => [novoLembrete,...lembretesAtuais])
-    setLembrete('')  
+    setLembretes(lembretesAtuais => [novoLembrete, ...lembretesAtuais])
+    setLembrete('')
+  }
+  const remover = (lembrete: Lembrete) => {
+    console.log('chamou a remover...')
+    setLembretes(
+      lembretesAtual => lembretesAtual.filter(item => item.id !== lembrete.id)
+    )
+    // Alert.alert(
+    //   // título
+    //   'Remover lembrete',
+    //   `Deseja remover esse lembrete? ${lembrete.texto}`,
+    //   [
+    //     {
+    //       text: 'Cancelar',
+    //       style: 'cancel'
+    //     },
+    //     {
+    //       text: 'Remover',
+    //       style: 'destructive',
+    //       onPress: () => {
+    //         setLembretes(
+    //           lembretesAtual => lembretesAtual.filter(item => item.id !== lembrete.id)
+    //         )
+    //       }
+    //     }
+    //   ]
+    // )
+
   }
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.input} 
+        style={styles.input}
         placeholder='Digite um lembrete...'
         onChangeText={setLembrete}
         value={lembrete}
       />
       <Pressable
+        onPress={adicionar}
         style={styles.button}>
         <Text
           style={styles.buttonText}>
           Salvar lembrete
         </Text>
       </Pressable>
+      <FlatList
+        style={styles.list}
+        keyExtractor={item => item.id}
+        data={lembretes}
+        renderItem={(lembrete => (
+          <View
+            style={styles.listItem}>
+            <Text
+              style={styles.listItemText}>
+              {lembrete.item.texto}
+            </Text>
+            <View
+              style={styles.listItemButtons}>
+              <Pressable
+                onPress={() => remover(lembrete.item)}>
+                <AntDesign
+                  name="delete"
+                  size={24} />
+              </Pressable>
+              <Pressable>
+                <AntDesign
+                  name="edit"
+                  size={24}
+                />
+              </Pressable>
+            </View>
+          </View>
+        ))} />
     </View>
   );
 }
@@ -57,6 +116,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 40
   },
   input: {
     width: '80%',
@@ -76,5 +136,32 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     textAlign: 'center'
+  },
+  list: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '80%',
+    marginTop: 12,
+    borderRadius: 4,
+    padding: 8
+  },
+  listItem: {
+    backgroundColor: '#F0F0F0',
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    marginBottom: 4,
+    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  listItemText: {
+    textAlign: 'center',
+    width: '70%',
+  },
+  listItemButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '30%'
   }
 });
